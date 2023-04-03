15c6856df110985afe216c61101f10824c2ce160
bizip <bizip04@gmail.com>
2023-04-03 16:53:34 +0200

    Add teacher class with one ovverider methods

---
+++ src/com/oop_school_library_java/Teacher/Teacher.java
@@ -0,0 +1,27 @@
+package com.oop_school_library_java.Teacher;
+
+import com.oop_school_library_java.Person;
+
+public class Teacher extends Person {
+	private String specialization;
+	
+	public Teacher(int id, int age, boolean parentPermission, String name, String specialization) {
+		super( id, age,parentPermission, name);
+		this.setSpecialization(specialization);
+	}
+	
+  @Override
+    public boolean canUseServices() {
+        return true;
+    }
+
+	public String getSpecialization() {
+		return specialization;
+	}
+
+	public void setSpecialization(String specialization) {
+		this.specialization = specialization;
+	}
+	
+
+}
